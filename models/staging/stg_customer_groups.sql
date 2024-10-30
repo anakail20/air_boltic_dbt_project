7d1df63513fc5d643b89c635130c8ccc06b0ed8e
anakail20 <anastasiakailani@gmail.com>
2024-10-30 14:04:45 +0100

    Add files via upload

---
+++ models/staging/stg_customer_groups.sql
@@ -0,0 +1,9 @@
+-- models/staging/stg_customer_group.sql
+{{ config(materialized='view') }}
+
+SELECT 
+    CAST("ID" AS INTEGER) AS customer_group_id,
+    "Type" AS customer_group_type,
+    "Name" AS customer_group_name,
+    "Registry number" AS registry_number
+FROM {{ source('raw_layer', 'customer_groups') }}
