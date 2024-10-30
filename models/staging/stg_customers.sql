d25265e6f4de9bff97c2eb0020baa2cb102c3839
anakail20 <anastasiakailani@gmail.com>
2024-10-30 14:12:22 +0100

    FIX: stg customers

--- models/staging/stg_customers.sql
+++ models/staging/stg_customers.sql
@@ -4,6 +4,6 @@ SELECT
     CAST("Customer ID" AS INTEGER) AS customer_id,
     "Name" AS customer_name,
     CAST("Customer Group ID" AS INTEGER) AS customer_group_id,
-    {{ safe_column("Email" "No email provided") }} AS email,
+    {{ safe_column("Email" , "No email provided") }} AS email,
     {{ safe_column("Phone Number", "No phone number provided") }} AS phone_number
 FROM {{ source('raw_layer', 'customers') }}
