8d6a2700ced4f490db58d82f39e52359327b5203
anakail20 <anastasiakailani@gmail.com>
2024-10-30 14:04:35 +0100

    Delete models/staging/staging directory

--- models/staging/staging/stg_customer_groups.sql
+++
@@ -1,9 +0,0 @@
--- models/staging/stg_customer_group.sql
-{{ config(materialized='view') }}
-
-SELECT 
-    CAST("ID" AS INTEGER) AS customer_group_id,
-    "Type" AS customer_group_type,
-    "Name" AS customer_group_name,
-    "Registry number" AS registry_number
-FROM {{ source('raw_layer', 'customer_groups') }}
--- models/staging/staging/stg_customers.sql
+++
@@ -1,9 +0,0 @@
-{{ config(materialized='view') }}
-
-SELECT 
-    CAST("Customer ID" AS INTEGER) AS customer_id,
-    "Name" AS customer_name,
-    CAST("Customer Group ID" AS INTEGER) AS customer_group_id,
-    "Email" AS customer_email,
-    "Phone Number" AS phone_number
-FROM {{ source('raw_layer', 'customers') }}
